8ca4130ec5fb0ec8822963c16f9454114079e873
unworking <victortic0910@gmail.com>
2022-03-26 11:23:28 +0100

    'commit'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import Layout from '../components/Layout';
-
-
-import styles from '../styles/Subscribe.module.css'
 import logo from '../../public/logo.png';
 
-const regex_mail = /^((([!#$%&' * +\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/
+import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
@@ -21,11 +18,10 @@ export default function Home() {
                 <meta name="description" content="Home blablabla"></meta>
             </Head>
 
-            <div className={styles.body}>
+            <div className={styles.container}>
 
 
                 <div className={styles.header}>
-
                     <div className={styles.header_img}>
                         <Image
                             src={logo}
@@ -36,12 +32,29 @@ export default function Home() {
 
                     <div className={styles.header_title}>
                         @Plant.zila
-
                     </div>
                     <div className={styles.header_text}>
                         Soy Plant.zila y me gustan las plantas y no sé qué de plantas.
                     </div>
                 </div>
+
+                <div className={styles.buttons_container}>
+                    <div className={styles.button_module}>
+                        Descarga el cultivo del philodendron baby (gratuito)
+                    </div>
+
+                    <div className={styles.button_module}>
+                        hazte con la guía EL LENGUANE DE LAS PLANTAS BABY (papel)
+                    </div>
+                    <div className={styles.button_module}>
+                        EL LENGUAJE DE LAS PLANRAS BABY (ebook)
+                    </div>
+                    <div className={styles.button_module}>
+                        Invítame a un café
+                    </div>
+                </div>
+
+
 
 
             </div>
